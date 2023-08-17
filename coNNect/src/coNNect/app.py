5bc2d5f83071f1a307c452605aa0371627799934
Amritanshu Shandilya <starkshivam1611@gmail.com>
2023-08-17 22:58:20 +0530

    Basic time code

--- coNNect/src/coNNect/app.py
+++ coNNect/src/coNNect/app.py
@@ -5,22 +5,40 @@ import toga
 from toga.style import Pack
 from toga.style.pack import COLUMN, ROW
 
+import time
+
 
 class MiniWatchConnect(toga.App):
 
     def startup(self):
-        """
-        Construct and show the Toga application.
+        main_box = toga.Box(style=Pack(direction=COLUMN))
 
-        Usually, you would add your application to a main content box.
-        We then create a main window (with a name matching the app), and
-        show the main window.
-        """
-        main_box = toga.Box()
+        time = toga.Label(
+            self.time,
+            style=Pack(padding=(0, 5))
+        )
+        # self.name_input = toga.TextInput(style=Pack(flex=1))
+
+        name_box = toga.Box(style=Pack(direction=ROW, padding=5))
+        name_box.add(time)
+        # name_box.add(self.name_input)
+
+        # button = toga.Button(
+        #     "Say Hello!",
+        #     on_press=self.say_hello,
+        #     style=Pack(padding=5)
+        # )
+        
+        main_box.add(name_box)
+        # main_box.add(button)
 
         self.main_window = toga.MainWindow(title=self.formal_name)
         self.main_window.content = main_box
         self.main_window.show()
+        
+    def time(self):
+        print(time.gmtime(0))
+        # return time.gmtime(0)
 
 
 def main():
