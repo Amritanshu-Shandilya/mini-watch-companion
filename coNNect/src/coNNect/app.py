b564477e04eb528c69c4090f3025a92110b56eae
Amritanshu Shandilya <starkshivam1611@gmail.com>
2023-08-19 04:31:22 +0530

    formatted the code

--- coNNect/src/coNNect/app.py
+++ coNNect/src/coNNect/app.py
@@ -13,31 +13,35 @@ class MiniWatchConnect(toga.App):
     def startup(self):
         main_box = toga.Box(style=Pack(direction=COLUMN))
         
-        # main_data = toga.Label(
-        #     self.fetch_time(),
+        # self.main_data = toga.Label(
+        #     id = "Data",
+        #     text=self.fetch_time(),
         #     style=Pack(padding=(0,5))
         # )
 
-        display_time = toga.Label(
-            "Time : "+self.fetch_time()[3],
+        self.display_time = toga.Label(
+            id="Time",
+            text="Time : "+self.fetch_time()[3],
             style=Pack(padding=(1, 5))
         )   
         
-        display_date = toga.Label(
-            "Date : " + self.fetch_time()[1]+" "+self.fetch_time()[2]+" "+self.fetch_time()[4],
+        self.display_date = toga.Label(
+            id="Date",
+            text="Date : " + self.fetch_time()[1]+" "+self.fetch_time()[2]+" "+self.fetch_time()[4],
             style=Pack(padding=(2,5))
         )
         
-        display_day = toga.Label(
-            "Day : " +  self.fetch_time()[0],
+        self.display_day = toga.Label(
+            id="Day",
+            text="Day : " +  self.fetch_time()[0],
             style=Pack(padding=(3,5))
         )
 
         name_box = toga.Box(style=Pack(direction=COLUMN, padding=5))
         # name_box.add(main_data)
-        name_box.add(display_time)
-        name_box.add(display_date)
-        name_box.add(display_day)
+        name_box.add(self.display_time)
+        name_box.add(self.display_date)
+        name_box.add(self.display_day)
 
         main_box.add(name_box)
         # main_box.add(button)
@@ -49,6 +53,7 @@ class MiniWatchConnect(toga.App):
     def fetch_time(self):
         time_val = time.localtime()
         data =  time.asctime(time_val)
+        
         return data.split(" ")
 
 
