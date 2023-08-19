f52dffc92cc56d6408908cb63c2d2b0ef8a63079
Amritanshu Shandilya <starkshivam1611@gmail.com>
2023-08-19 06:48:28 +0530

    Added basic styling to the data

--- coNNect/src/coNNect/app.py
+++ coNNect/src/coNNect/app.py
@@ -3,9 +3,11 @@ A smartphone companion app for MiniWatch project on Github
 """
 import toga
 from toga.style import Pack
-from toga.style.pack import COLUMN, ROW
+from toga.style.pack import COLUMN, CENTER
+
 
 import time
+import asyncio
 
 
 class MiniWatchConnect(toga.App):
@@ -15,13 +17,12 @@ class MiniWatchConnect(toga.App):
         local_time = time.localtime()
         return time.asctime(local_time).split(" ")
     
-    # async
     def update_time(self):
         """This will be used to update the time only"""
         pass
 
     def startup(self):
-        main_box = toga.Box(style=Pack(direction=COLUMN))
+        main_box = toga.Box(style=Pack(direction=COLUMN, alignment = CENTER))
         
         # self.main_data = toga.Label(
         #     id = "Data",
@@ -32,24 +33,24 @@ class MiniWatchConnect(toga.App):
         self.display_time = toga.Label(
             id="Time",
             text="Time : "+ self.fetch_data()[3],
-            style=Pack(padding=(1, 5))
+            style=Pack(padding=3, text_align = CENTER)
         )   
         
         self.display_date = toga.Label(
             id="Date",
             text="Date : " + self.fetch_data()[2]+" "+self.fetch_data()[1]+" "+self.fetch_data()[4],
-            style=Pack(padding=(2,5))
+            style=Pack(padding=3, text_align = CENTER)
         )
         
         self.display_day = toga.Label(
             id="Day",
             text="Day : " +  self.fetch_data()[0],
-            style=Pack(padding=(3,5))
+            style=Pack(padding=3, text_align = CENTER)
         )
         
         self.add_background_task(self.update_time())
 
-        name_box = toga.Box(style=Pack(direction=COLUMN, padding=5))
+        name_box = toga.Box(style=Pack(direction=COLUMN, padding=5, alignment = CENTER))
         # name_box.add(self.main_data)
         name_box.add(self.display_time)
         name_box.add(self.display_date)
